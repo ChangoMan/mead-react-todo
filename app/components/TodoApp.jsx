85daccac6fe57a2c801405ec6c9df4c4f7f00d6c
Hunter Chang <hunter@madwiremedia.com>
2017-04-03 14:38:34 -0600

    add AddTodo

--- app/components/TodoApp.jsx
+++ app/components/TodoApp.jsx
@@ -1,5 +1,6 @@
 var React = require('react');
 var TodoList = require('TodoList');
+var AddTodo = require('AddTodo');
 
 var TodoApp = React.createClass({
     getInitialState: function () {
@@ -19,6 +20,9 @@ var TodoApp = React.createClass({
             }]
         }
     },
+    handleAddTodo: function (text) {
+        alert('new todo: ' + text)
+    },
     render: function () {
 
         var {todos} = this.state;
@@ -26,6 +30,7 @@ var TodoApp = React.createClass({
         return (
             <div>
                 <TodoList todos={todos} />
+                <AddTodo onAddTodo={this.handleAddTodo} />
             </div>
         )
   }
